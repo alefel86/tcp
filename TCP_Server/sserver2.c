f540c8ff57f99ef33414d74a57250e48b1495a98
Alexander Feldinger <ic17b055@technikum-wien.at>
2018-12-04 20:47:47 +0100

    Copy aus BeeJ

--- TCP_Server/sserver2.c
+++ TCP_Server/sserver2.c
@@ -24,15 +24,10 @@
  // errno = saved_errno;
 // }
 // get sockaddr, IPv4 or IPv6:
-void getPort(int argc, char *argv[], char** server_port);
 
-void *get_in_addr(struct sockaddr *sa)
-{
- if (sa->sa_family == AF_INET) {
- return &(((struct sockaddr_in*)sa)->sin_addr);
- }
- return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
+void getPort(int argc, char *argv[], char** server_port);
+void *get_in_addr(struct sockaddr *sa);
+
 int main(int argc, char *argv[]) 
 {
 	char *server_port;
@@ -52,23 +47,28 @@ int main(int argc, char *argv[])
  hints.ai_family = AF_UNSPEC;
  hints.ai_socktype = SOCK_STREAM;
  hints.ai_flags = AI_PASSIVE; // use my IP
- if ((rv = getaddrinfo(NULL, server_port, &hints, &servinfo)) != 0) {
+ 
+ if ((rv = getaddrinfo(NULL, server_port, &hints, &servinfo)) != 0) 
+ {
  fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
  return 1;
  }
  // loop through all the results and bind to the first we can
- for(p = servinfo; p != NULL; p = p->ai_next) {
- if ((sockfd = socket(p->ai_family, p->ai_socktype,
- p->ai_protocol)) == -1) {
+ for(p = servinfo; p != NULL; p = p->ai_next) 
+ {
+ if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) 
+ {
  perror("server: socket");
  continue;
  }
  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
- sizeof(int)) == -1) {
+ sizeof(int)) == -1) 
+ {
  perror("setsockopt");
  exit(1);
  }
- if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+ if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) 
+ {
  close(sockfd);
  perror("server: bind");
  continue;
@@ -76,11 +76,13 @@ int main(int argc, char *argv[])
  break;
  }
  freeaddrinfo(servinfo); // all done with this structure
- if (p == NULL) {
+ if (p == NULL) 
+ {
  fprintf(stderr, "server: failed to bind\n");
  exit(1);
  }
- if (listen(sockfd, BACKLOG) == -1) {
+ if (listen(sockfd, BACKLOG) == -1) 
+ {
  perror("listen");
  exit(1);
  }
@@ -88,7 +90,8 @@ int main(int argc, char *argv[])
  
  sigemptyset(&sa.sa_mask);
  sa.sa_flags = SA_RESTART;
- if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+ if (sigaction(SIGCHLD, &sa, NULL) == -1) 
+ {
  perror("sigaction");
  exit(1);
  }
@@ -96,15 +99,18 @@ int main(int argc, char *argv[])
  while(1) { // main accept() loop
  sin_size = sizeof their_addr;
  new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
- if (new_fd == -1) {
+ if (new_fd == -1) 
+ {
  perror("accept");
  continue;
  }
+ 
  inet_ntop(their_addr.ss_family,
  get_in_addr((struct sockaddr *)&their_addr),
  s, sizeof s);
  printf("server: got connection from %s\n", s);
- if (!fork()) { // this is the child process
+ if (!fork()) 
+ { // this is the child process
  close(sockfd); // child doesn't need the listener
  if (send(new_fd, "Hello, world!", 13, 0) == -1)
  perror("send");
@@ -134,8 +140,6 @@ void getPort(int argc, char *argv[], char** server_port)
 			//printUsage();
 			exit(EXIT_FAILURE);
 		}
-
-		
 		
 		/*optstring is a string containing the legitimate option characters.
        If such a character is followed by a colon, the option requires an
@@ -164,4 +168,12 @@ void getPort(int argc, char *argv[], char** server_port)
 		//printUsage();
 		exit(EXIT_FAILURE);
 	}
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+ if (sa->sa_family == AF_INET) {
+ return &(((struct sockaddr_in*)sa)->sin_addr);
+ }
+ return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }

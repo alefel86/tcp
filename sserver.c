9c2d5e98cf6312ab3f562f6e466d755e28d43dd0
manuel <ic17b022@technikum-wien.at>
2018-12-10 21:38:07 +0100

    move socket opening from main to function minor touch ups

--- sserver.c
+++ sserver.c
@@ -22,8 +22,9 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <stdbool.h>
 
-#define BACKLOG 10 
+#define BACKLOG 10 // how many pending connections queue will hold
 #define MAX_BUFFER (1500)
 
 // to-do:
@@ -33,124 +34,71 @@ void getPort(int argc, char *argv[], char **server_port);
 void sigchld_handler(int s);
 void printUsage(void);
 
+bool open_socket(const char* server_port, int* socket_fd);
+
 const char *program_name = NULL;
 
 int main(int argc, char *argv[])
 {
+    bool rc = true;
 	char *server_port;
 	char in_buff[MAX_BUFFER];
 	program_name = argv[0];
-	int sfd, new_fd; 
-	struct addrinfo hints, *result, *rp;
-	struct sockaddr_storage client_addr; 
-	socklen_t client_size;
+    int sfd, new_fd;
+    struct sockaddr remote_addr;
+    socklen_t sin_size;
 	struct sigaction sa;
-	int yes = 1;
-	int s;
 
 	getPort(argc, argv, &server_port);
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; 
+    rc = rc && open_socket(server_port, &sfd);
 
-	s = getaddrinfo(NULL, server_port, &hints, &result);
-	if (s != 0)
-	{
-		fprintf(stderr, "%s::getaddrinfo: %s\n", program_name, gai_strerror(s));
-		exit(EXIT_FAILURE);
-	}
-	
-	for (rp = result; rp != NULL; rp = rp->ai_next)
-	{
-		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-		if (sfd == -1)
-		{
-			fprintf(stderr, "%s::Socket: %s\n", program_name, strerror(errno));
-			continue;
-		}
-		
-		if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-		{
-			fprintf(stderr, "%s::setSocket: %s\n", program_name, strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == -1)
-		{
-			close(sfd);
-
-			fprintf(stderr, "%s::bind: %s\n",program_name, strerror(errno));
-			continue;
-		}
-		break;
-	}
-	freeaddrinfo(result); 
-
-	if (rp == NULL)
-	{
-
-		fprintf(stderr, "%s::Bind: %s\n", program_name, strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	if (listen(sfd, BACKLOG) == -1)
-	{
-		close(sfd);
-		fprintf(stderr, "%s::Listen: %s\n", program_name, strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	sa.sa_handler = sigchld_handler; 
+    sa.sa_handler = sigchld_handler;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1)
 	{
 		fprintf(stderr, "%s::Signal: %s\n", program_name, strerror(errno));
-		close(sfd);
-		exit(EXIT_FAILURE);
-	}
+        rc = false;
+    }
 
-	while (1)
-	{ 
-		client_size = sizeof client_addr;
-		new_fd = 0;
-		new_fd = accept(sfd, (struct sockaddr *)&client_addr, &client_size);
+    while (rc) {
+        sin_size = sizeof(remote_addr);
+        new_fd = accept(sfd, &remote_addr, &sin_size);
 		if (new_fd == -1)
 		{
 			fprintf(stderr, "%s::Accept: %s\n", program_name, strerror(errno));
-			close(sfd);
-			exit(EXIT_FAILURE);
-		}
-		
-		int child_id;
-		child_id = fork();
-		switch (child_id)
-		{
-		case -1: 
-			close(sfd);
-			close(new_fd);
-			fprintf(stderr, "%s::Child create: %s\n", program_name, strerror(errno));
-			exit(EXIT_FAILURE);
-			break;
-		case 0: //child
+            rc = false;
+        } else {
+            int child_id;
+            child_id = fork();
+            switch (child_id) {
+                case -1:
+                    close(new_fd);
+                    fprintf(stderr, "%s::Child create: %s\n", program_name, strerror(errno));
+                    rc = false;
+                    break;
+                case 0: //child
 
-			close(sfd);
-			memset(in_buff, 0, MAX_BUFFER * sizeof(in_buff[0]));
-			dup2(new_fd, STDOUT_FILENO);
-			dup2(new_fd, STDIN_FILENO);
-			close(new_fd);
+                    close(sfd);
+                    memset(in_buff, 0, MAX_BUFFER * sizeof(in_buff[0]));
+                    dup2(new_fd, STDOUT_FILENO);
+                    dup2(new_fd, STDIN_FILENO);
+                    close(new_fd);
 
-			execlp("simple_message_server_logic", "simple_message_server_logic", (char *)NULL);
-			fprintf(stderr, "%s::exec: %s\n", program_name, strerror(errno));
-			exit(EXIT_FAILURE);
-
-			break;
-		default: //parent
-			close(new_fd);
-			break;
+                    execlp("simple_message_server_logic", "simple_message_server_logic", (char*) NULL);
+                    fprintf(stderr, "%s::exec: %s\n", program_name, strerror(errno));
+                    exit(EXIT_FAILURE);
+                default: //parent
+                    close(new_fd);
+                    break;
+            }
 		}
 	}
-	return 0;
+
+    close(sfd);
+    exit(EXIT_FAILURE);    //can't get here with rc == true
 }
 
 void getPort(int argc, char *argv[], char **server_port)
@@ -189,14 +137,10 @@ void getPort(int argc, char *argv[], char **server_port)
 
 void sigchld_handler(int s)
 {
-	// waitpid() might overwrite errno, so we save and restore it
+    s++; //quiet now, compiler
+    // waitpid() might overwrite errno, so we save and restore it:
 	int saved_errno = errno;
-	if (s)
-	{
-	} 
 	while (waitpid(-1, NULL, WNOHANG) > 0)
-	{
-	}
 	errno = saved_errno;
 }
 
@@ -207,4 +151,53 @@ void printUsage()
 	fprintf(stderr, "options:\n");
 	fprintf(stderr, "\t-p, --port <port>\n");
 	fprintf(stderr, "\t-h, --help\n");
+}
+
+bool open_socket(const char* server_port, int* socket_fd) {
+    bool rc = true;
+    struct addrinfo hints, * result, * address;
+    int ret, sfd = -1;
+    int yes = 1;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    ret = getaddrinfo(NULL, server_port, &hints, &result);
+    if (ret != 0) {
+        fprintf(stderr, "%s::getaddrinfo: %s\n", program_name, gai_strerror(ret));
+        rc = false;
+    }
+
+    for (address = result; rc && address != NULL; address = address->ai_next) {
+        sfd = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
+        if (sfd == -1) {
+            fprintf(stderr, "%s::Socket: %s\n", program_name, strerror(errno));
+            continue;
+        }
+
+        if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            fprintf(stderr, "%s::setSocket: %s\n", program_name, strerror(errno));
+            rc = false;
+        }
+        if (bind(sfd, address->ai_addr, address->ai_addrlen) == -1) {
+            close(sfd);
+            continue;
+        }
+        break;
+    }
+    freeaddrinfo(result);
+
+    if (address == NULL) {
+        fprintf(stderr, "%s::Bind: %s\n", program_name, strerror(errno));
+        rc = false;
+    } else if (listen(sfd, BACKLOG) == -1) {
+        fprintf(stderr, "%s::Listen: %s\n", program_name, strerror(errno));
+        rc = false;
+    }
+
+    *socket_fd = sfd;
+
+    return rc;
 }
